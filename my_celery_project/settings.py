1af7ab742dea169824ff08ffdeff028825ffd22f
Sunil  Kunkekar <sunilkunkekar@percipient.in>
2025-03-12 18:28:52 +0530

    Implement the template files and views functions for celery

--- my_celery_project/settings.py
+++ my_celery_project/settings.py
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Celery Settings
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+# CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 # CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
-#CELERY_RESULT_BACKEND = 'django-db'
+# CELERY_RESULT_BACKEND = 'django-db'
 CELERY_TIMEZONE = "Asia/Kolkata"
--- myapp/views.py
+++ myapp/views.py
@@ -7,3 +7,23 @@ def index(request):
     result1 = add.delay(10,42)
     print("result1:",result1)
     return render(request, "myapp/home.html")
+
+# # Display addition value after task execution
+# def index(request):
+#     result = add.delay(10, 30)
+#     return render(request, "myapp/home.html", {'result':result})
+
+# def check_result(request, task_id):
+#     # Retrieve the task result using the task_id
+#     result = AsyncResult(task_id)
+#     # print("Ready: ", result.ready())
+#     # print("Successful: ", result.successful())
+#     # print("Failed: ", result.failed())
+#     # print("Get: ", result.get())
+#     return render(request, 'myapp/result.html', {'result':result})
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def contact(request):
+    return render(request, 'myapp/contact.html')
